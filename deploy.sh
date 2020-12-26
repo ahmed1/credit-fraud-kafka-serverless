c880a7d54fb4de204f4cd0fd19ebf004e170716e
Ahmed Shoukr <ahmedshoukr@Ahmeds-MacBook-Pro-2.local>
2020-12-26 14:48:14 -0800

    added dynamo table to template

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ sam build --use-container --template-file template.yaml
 sam package --s3-bucket cc-final-project-cloudformation --output-template-file packaged.yaml
 
 sam deploy --template-file ./packaged.yaml --stack-name cc-final-project-cloudformation-stack --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+
