df58f5e722fef83ede4fb4054e6906079fdb81e2
Ahmed Shoukr <ahmedshoukr@Ahmeds-MacBook-Pro-2.local>
2020-12-27 06:22:13 -0800

    added routing for corresponding topic based on request

--- producer-t1-t2/producer_t1_t2.py
+++ producer-t1-t2/producer_t1_t2.py
@@ -6,20 +6,23 @@ import base64
 
 
 def lambda_handler(event, context):
+    
+    print('EVENT:', event) # for logging / debugging
+
    
    
     producer = KafkaProducer(security_protocol="SSL", bootstrap_servers = ['b-2.kafkacluster4.quil2t.c11.kafka.us-east-1.amazonaws.com:9094',\
                                                                             'b-1.kafkacluster4.quil2t.c11.kafka.us-east-1.amazonaws.com:9094'])
 
-    print(producer.bootstrap_connected())
+    print(producer.bootstrap_connected()) # make sure can establish connection with kafka
     
-    message = event
-    print(message)
-    message = str(message)
+    topic = event['resource'][1:]
+    message = json.loads(event['body'])['body']
+    print('MESSAGE:', message)
     message_bytes = message.encode('utf-8')
     #base64_bytes = base64.b64encode(message_bytes)
     
-    response = producer.send(topic='authorize_purchase', value = message_bytes)
+    response = producer.send(topic=topic, value = message_bytes)
     
     print('response', response)
     
