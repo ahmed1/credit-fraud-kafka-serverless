5125d7a2d0cd2f8e953e30ae9d8d5e15c2fe77e5
Ahmed Shoukr <ahmedshoukr@Ahmeds-MacBook-Pro-2.local>
2020-12-27 03:19:34 -0800

    updated consumer t1 to decode kafka messages

--- consumer-t1/consumer_t1.py
+++ consumer-t1/consumer_t1.py
@@ -1,10 +1,13 @@
 import json
-
+import base64
 # import requests
 
 
 def lambda_handler(event, context):
     print('event:', event)
+
+    message = message.encode('utf-8')
+    message = base64.decodebytes(message)
     
     messages = [event['records'][x][0]['value'] for x in event['records'].keys()]
     for message in messages:
