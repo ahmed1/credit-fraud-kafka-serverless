108370d0742a3084aebc9b0fd818aada9119acc3
CarolineNB <eniloracb4@gmail.com>
2020-12-27 18:35:51 -0500

    add denied transaction table schema

--- consumer-t3/consumer_t3.py
+++ consumer-t3/consumer_t3.py
@@ -8,25 +8,31 @@ from decimal import Decimal
 dynamo = boto3.resource('dynamodb')
 rejected = dynamo.Table('rejected-transactions')
 
-def to_decimal(message):
-    message['amt'] = Decimal(str(float(message['amt'])))
-    message['lat'] = Decimal(str(float(message['lat'])))
-    message['long'] = Decimal(str(float(message['long'])))
-    message['merch_lat'] = Decimal(str(float(message['merch_lat'])))
-    message['merch_long'] = Decimal(str(float(message['merch_long'])))
-    message['date'] = message['trans_date_trans_time']
-    return message 
-
-
 def lambda_handler(event, context):
     messages = [event['records'][key][0]['value'] for key in event['records'].keys()]
     messages = [message.encode('utf-8') for message in messages]
     messages = [base64.decodebytes(message) for message in messages]
     messages = [eval(message) for message in messages] # json objects you can write to dynamodb
+     
     
     for message in messages:
-        transaction = purchases.put_item(Item=to_decimal(message))
-        rejected.put_item(transaction)
+        message['date'] = message['trans_date_trans_time'].split(" ")[0]
+        
+        try:
+            day = day.get_item(Key={'date': message['date']})
+            day['Item']
+        except:
+            curr_user = None
+
+        if curr_user == None:
+            item = {
+                'date': message['date'],
+                'uuids': [message['uuid']]
+            }
+            day.put_item(Item=item)
+        else:
+            day['uuids'].append(message['uuid'])
+            rejected.put_item(Item=day)
 
     
     print('processed messages:', messages)
